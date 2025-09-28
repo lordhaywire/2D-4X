9ba90f494772a51a28937dc3042b91e91ed0d4a1
lordhaywire <lordhaywire@gmail.com>
2025-09-28 12:57:51 -0700

    New scene got missed in last push.

---
+++ Scripts/UI/DeadPeopleHBoxContainer.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+public partial class DeadPeopleHBoxContainer : HBoxContainer
+{
+    [Export] public Label deadPersonNameLabel;
+    [Export] public Label deadPersonLocationLabel;
+    [Export] public Label deadPersonCauseOfDeathLabel;
+}
